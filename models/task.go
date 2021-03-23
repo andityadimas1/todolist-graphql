23f4b0f499e0014f715b4851268468744e2a65d5
andityadimas1 <dimas.aninditya@xapiens.id>
2021-03-23 16:03:09 +0700

    edit models

--- models/task.go
+++ models/task.go
@@ -8,13 +8,13 @@ import (
 
 type Task struct {
 	gorm.Model
-	ID          uint       `json:"id"`
-	TaskNama    string     `json:"tasknama"`
-	Completed   string     `json:"completed"`
-	IsiListData []ListData `gorm:"foreignKey:ListData"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	ID        uint   `json:"id"`
+	TaskNama  string `json:"tasknama"`
+	Completed string `json:"completed"`
+	// IsiListData []ListData `gorm:"foreignKey:ListData"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 
 	// IdList uint `gorm :"foreignKey:"id" json: Idlist`
 	// Created   time.Time `json:"created at"`
